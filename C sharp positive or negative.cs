5192814a0f8a64e2f012f5a3e1860b2393fd1580
Abhishek_7 <abhisheknshetty656@gmail.com>
2025-07-31 15:07:50 +0530

    Add files via upload

---
+++ C sharp positive or negative.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out double number))
+        {
+            if (number > 0)
+                Console.WriteLine("The number is Positive.");
+            else if (number < 0)
+                Console.WriteLine("The number is Negative.");
+            else
+                Console.WriteLine("The number is");
+            double square = number * number;
+            Console.WriteLine($"The square of {number} is {square}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+        }
+
+        Console.WriteLine("Press any key");
+        Console.ReadKey();
+    }
+}
+
